b929d37f7c9877684410b18fb6efac946d149a4a
Edgar Carballo <karvayoEdgar@gmail.com>
2015-04-25 15:42:39 -0500

    Se agrega gráficas sin peso.
    También se implementa IGrafica: interface suficiente para usar el
    "poder" de las gráficas.

--- Graficas.cs
+++ Graficas.cs
@@ -5,11 +5,29 @@ using ListasExtra;
 namespace Graficas
 {
 	/// <summary>
+	/// PRomete lista de vecinos.
+	/// </summary>
+	public interface IGrafica<T>
+	{
+		T[] Nodos
+		{
+			get;
+		}
+
+		T[] Vecinos(T nodo);
+	}
+
+	/// <summary>
 	/// Representa una gráfica, en el sentido abstracto.
 	/// Los nodos serán del tipo <c>T</c>.
 	/// </summary>
-	public class Grafica<T>
+	public class Grafica<T>: IGrafica<T>
 	{
+		T[] Graficas.IGrafica<T>.Vecinos(T nodo)
+		{
+			return Vecino(nodo).ToArray();
+		}
+
 		/// <summary>
 		/// Initializes a new instance of the <see cref="Graficas.Grafica`1"/> class.
 		/// </summary>
@@ -435,4 +453,127 @@ namespace Graficas
 		}
 	}
 
+	public class GraficaNoPeso<T>:IGrafica<T>
+	{
+		class Nodo
+		{
+			public T obj;
+			public List<T> Vecinos = new List<T>();
+
+			public Nodo(T nod)
+			{
+				obj = nod;
+			}
+		}
+		/// <summary>
+		/// Devuelve la lista de nodos.
+		/// </summary>
+		/// <returns>The nodos.</returns>
+		public T[] getNodos()
+		{
+			int num = nodos.Count;
+			T[] ret = new T[num];
+			for (int i = 0; i < num; i++)
+			{
+				ret[i] = nodos[i].obj;
+			}
+			return ret;
+		}
+
+		List<Nodo> nodos = new List<Nodo>();
+		T[] IGrafica<T>.Nodos 
+		{
+			get
+			{
+				return getNodos();
+			}
+		}
+		/// <summary>
+		/// Devuelve la lista de vecinos de un nodo.
+		/// </summary>
+		/// <param name="nodo">Nodo.</param>
+		public T[] Vecinos (T nodo)
+		{
+			return nodos.Find(x => x.Equals(nodo)).Vecinos.ToArray();
+		}
+
+		/// <summary>
+		/// Devuelve un arreglo con los vecinos de un nodo específico.
+		/// </summary>
+		/// <param name="nodo">Nodo.</param>
+		public T[] this [T nodo]
+		{
+			get
+			{
+				return nodos.Find(x => x.obj.Equals(nodo)).Vecinos.ToArray();
+			}
+		}
+
+		public void AgregaNodo(T nodo)
+		{
+			// Resiva si existe
+			if (nodos.Exists(x => x.obj.Equals(nodo)))
+				throw new Exception("Nodo ya existente.");
+			nodos.Add(new Nodo(nodo));
+		}
+
+		/// <summary>
+		/// Devuelve el nodo que le corresponde a un objeto tipo T.
+		/// </summary>
+		/// <returns>The nodo.</returns>
+		/// <param name="nod">Nod.</param>
+		Nodo getNodo (T nod)
+		{
+			return nodos.Find(x => x.obj.Equals(nod));
+		}
+
+		/// <summary>
+		/// Agrega un vértice dado su origen y final.
+		/// </summary>
+		/// <param name="desde">Origen.</param>
+		/// <param name="hasta">Destino.</param>
+		public void AgregaVertice (T desde, T hasta)
+		{
+			Nodo des = getNodo(desde);
+			des.Vecinos.Add(hasta);
+		}
+	}
+
+	
+	[Serializable]
+	public class NodoInexistenteException : Exception
+	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="T:NodoInexistenteException"/> class
+		/// </summary>
+		public NodoInexistenteException ()
+		{
+		}
+		
+		/// <summary>
+		/// Initializes a new instance of the <see cref="T:NodoInexistenteException"/> class
+		/// </summary>
+		/// <param name="message">A <see cref="T:System.String"/> that describes the exception. </param>
+		public NodoInexistenteException (string message) : base (message)
+		{
+		}
+		
+		/// <summary>
+		/// Initializes a new instance of the <see cref="T:NodoInexistenteException"/> class
+		/// </summary>
+		/// <param name="message">A <see cref="T:System.String"/> that describes the exception. </param>
+		/// <param name="inner">The exception that is the cause of the current exception. </param>
+		public NodoInexistenteException (string message, Exception inner) : base (message, inner)
+		{
+		}
+		
+		/// <summary>
+		/// Initializes a new instance of the <see cref="T:NodoInexistenteException"/> class
+		/// </summary>
+		/// <param name="context">The contextual information about the source or destination.</param>
+		/// <param name="info">The object that holds the serialized object data.</param>
+		protected NodoInexistenteException (System.Runtime.Serialization.SerializationInfo info, System.Runtime.Serialization.StreamingContext context) : base (info, context)
+		{
+		}
+	}
 }
