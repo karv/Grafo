441cf6dc59cb4446691c7d9c4d6eafcb8df30ed5
Edgar Carballo <karvayoEdgar@gmail.com>
2015-05-26 15:01:17 -0500

    clase ConjuntoNodos
    Es una lista de IHardNodes equivalente a un IGrafica

---
+++ ConjuntoNodos.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using Graficas;
+
+namespace Graficas.Nodos
+{
+	/// <summary>
+	/// Representa el conjunto de nodos de una gráfica.
+	/// </summary>
+	public class ConjuntoNodos<T> :  IEnumerable<IHardNodo<T>>, IGrafica<T>, ICollection<T>
+	// where T : IEquatable<T>
+	{
+		List<IHardNodo<T>> _dat = new List<IHardNodo<T>>();
+
+		public ConjuntoNodos() : base()
+		{
+			//_nodos = new List<IHardNodo<T>>();
+		}
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="Graficas.Nodos.ConjuntoNodos`1"/> class.
+		/// </summary>
+		/// <param name="graf">Gráfica de dónde copiar la información.</param>
+		public ConjuntoNodos(IGrafica<T> graf) : this()
+		{
+			// Primero crear los nodos
+			foreach (var x in graf.Nodos)
+			{
+				Add(x);
+			}
+			foreach (var x in _dat)
+			{
+				foreach (var y in graf.Vecinos(x.getObjeto))
+				{
+					x.getSucc.Add(this[y]);
+				}
+			}
+		}
+
+		//List<IHardNodo<T>> _nodos;
+
+		#region IGrafica implementation
+
+		IEnumerable<T> IGrafica<T>.Vecinos(T nodo)
+		{
+			throw new NotImplementedException();
+		}
+
+		IEnumerable<T> IGrafica<T>.Nodos
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		#endregion
+
+		#region IEnumerable implementation
+
+		public IEnumerator<IHardNodo<T>> GetEnumerator()
+		{
+			throw new NotImplementedException();
+		}
+
+		IEnumerator<T> IEnumerable<T>.GetEnumerator()
+		{
+			throw new NotImplementedException();
+		}
+
+		#endregion
+
+		#region IEnumerable implementation
+
+		System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+		{
+			throw new NotImplementedException();
+		}
+
+		#endregion
+
+		public IHardNodo<T> this [T Key]
+		{
+			get
+			{
+				try
+				{
+					return _dat.Find(x => x.getObjeto.Equals(Key));
+				}
+				finally
+				{
+					
+				}
+			}
+		}
+
+		#region ICollection
+
+		public void Add(T item)
+		{
+			
+			if (Contains(item))
+				throw new Exception("Ya se encuentra nodo.");
+
+			this._dat.Add(new HardNodo<T>(item));
+		}
+
+		public void Clear()
+		{
+			_dat.Clear();
+		}
+
+		public bool Contains(T item)
+		{
+			return _dat.Exists(x => x.getObjeto.Equals(item));
+		}
+
+		public void CopyTo(T[] array, int arrayIndex)
+		{
+			throw new NotImplementedException();
+		}
+
+		public bool Remove(T item)
+		{
+			return _dat.RemoveAll(x => x.getObjeto.Equals(item)) > 0;
+		}
+
+		public int Count
+		{
+			get
+			{
+				return _dat.Count;
+			}
+		}
+
+		public bool IsReadOnly
+		{
+			get
+			{
+				return false;
+			}
+		}
+
+		#endregion
+	}
+
+	public interface IHardNodo<T> : INodo<T>
+	{
+		new ICollection<IHardNodo<T>> getSucc { get; }
+		//void SetSucc (IEnumerator<IHardNodo> )
+	}
+}
+
---
+++ Nodo.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Graficas
+{
+	public class Nodo<T>: INodo<T>
+	{
+		public Nodo(T obj)
+		{
+			_obj = obj;
+			_succ = new List<T>();
+		}
+
+		/// <summary>
+		/// Construye un nodo que esté sincronizado a un "nodo" de una gráfica dada.
+		/// </summary>
+		/// <param name="obj">Objeto del nodo.</param>
+		/// <param name="graf">Gráfica donde se encuentra el nodo.</param>
+		public Nodo(T obj, IGrafica<T> graf)
+		{
+			_obj = obj;
+			_succ = graf.Vecinos(obj);
+		}
+
+		T _obj;
+		IEnumerable<T> _succ;
+
+		#region INodo implementation
+
+		T INodo<T>.getObjeto
+		{
+			get
+			{
+				return _obj;
+			}
+		}
+
+		System.Collections.Generic.IEnumerable<T> INodo<T>.getSucc
+		{
+			get
+			{
+				return (IEnumerable<T>)_succ;
+			}
+		}
+
+		#endregion
+	}
+}
+
