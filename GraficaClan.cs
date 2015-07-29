202688f4eff6c82cbba5a6e457c9493859e4bc44
Edgar Carballo <karvayoEdgar@gmail.com>
2015-07-29 16:53:27 -0500

    Empezandoa  trabajar en gráfica de clanes

---
+++ GraficaClan.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Graficas;
+using System.Collections.Generic;
+
+namespace Graficas
+{
+	public class GraficaClan<T>: IGrafica<T>
+	{
+		/// <summary>
+		/// Representa una gráfica modelada como conjunto de sus subgráficas completas maximales
+		/// </summary>
+		public GraficaClan()
+		{
+		}
+
+		#region IGrafica implementation
+
+		public System.Collections.Generic.ICollection<T> Vecinos(T nodo)
+		{
+			throw new NotImplementedException();
+		}
+
+		public bool ExisteArista(IArista<T> aris)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Graficas.Rutas.IRuta<T> toRuta(IEnumerable<T> seq)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Graficas.Rutas.IRuta<T> RutaOptima(T x, T y)
+		{
+			throw new NotImplementedException();
+		}
+
+		public System.Collections.Generic.ICollection<T> Nodos
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		#endregion
+	}
+}
+
