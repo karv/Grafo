7eb77ea5fa241adb2824b9b9e71332636342495f
Edgar Carballo <karvayoEdgar@gmail.com>
2015-10-05 12:44:03 -0500

    Agregar proyecto pruebas

---
+++ GrafosTest/GeneralTest.cs
@@ -0,0 +1,40 @@
+﻿using Graficas;
+using NUnit.Framework;
+using System;
+
+namespace Test
+{
+	[TestFixture]
+	public class GeneralTest
+	{
+		
+		public void GeneraGraficaConexa(IGrafica<int> gr, int cant = 100)
+		{
+			for (int i = 1; i <= cant; i++)
+			{
+				gr.AgregaArista(0, i);
+				//gr.AgregaArista(i, 0);
+			}
+			Assert.AreEqual(cant + 1, gr.Nodos.Count);
+
+		}
+
+		[Test]
+		public void CaminoOptimo()
+		{
+			var gr = new Grafica<int>();
+			gr.EsSimetrico = true;
+
+			GeneraGraficaConexa(gr);
+			var ruta = gr.CaminoÓptimo(2, 3);
+			Console.WriteLine(ruta);
+			Console.WriteLine(ruta.Longitud);
+
+			foreach (var x in ruta.Pasos)
+			{
+				Console.WriteLine(x);
+			}
+		}
+	}
+}
+
---
+++ GrafosTest/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Graficas;
+using System.Collections.Generic;
+
+namespace GrafosTest
+{
+	class MainClass
+	{
+		public static void Main2(string[] args)
+		{
+			Grafica<int> g = new Grafica<int>();
+			g.EsSimetrico = true;
+
+			GraficaClan<int> gr = new GraficaClan<int>();
+			gr.AgregaArista(0, 1);
+			gr.AgregaArista(0, 2);
+			gr.AgregaArista(1, 2);
+			gr.AgregaArista(0, 4);
+			if (gr.ExisteArista(new Arista<int>(1, 2, 1)))
+				Console.WriteLine("Hi");
+		}
+	}
+}
