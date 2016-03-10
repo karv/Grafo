2b954ecf6f31824ee6773e2dd9cbf1ba8eab6be2
Edgar Carballo <karvayoEdgar@gmail.com>
2016-03-10 16:19:15 -0600

    Agregar grafo como colección de IArista AristaColGrafo

---
+++ Grafo/AristaColGrafo.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using Graficas;
+using System.Collections.Generic;
+using Graficas.Rutas;
+
+namespace Graficas
+{
+	/// <summary>
+	/// Representa un grafo como una colección de aristas
+	/// </summary>
+	public class GrafoAristas<T> : IGrafoPeso<T>
+		where T : IEquatable<T>
+	{
+		public GrafoAristas ()
+		{
+			AristasColección = new List<IArista<T>> ();
+		}
+
+		protected ICollection<IArista<T>> AristasColección;
+
+		#region Grafo
+
+		public void Clear ()
+		{
+			AristasColección.Clear ();
+		}
+
+		public ICollection<IArista<T>> Aristas ()
+		{
+			return AristasColección;
+		}
+
+		public ICollection<T> Vecinos (T nodo)
+		{
+			var ret = new HashSet<T> ();
+			foreach (var x in AristasColección)
+			{
+				if (x.Origen.Equals (nodo))
+					ret.Add (x.Destino);
+				if (x.Destino.Equals (nodo))
+					ret.Add (x.Origen);
+			}
+			return ret;
+		}
+
+		public IRuta<T> ToRuta (IEnumerable<T> seq)
+		{
+			throw new NotImplementedException ();
+		}
+
+		public ILecturaGrafo<T> Subgrafo (IEnumerable<T> conjunto)
+		{
+			var col = new HashSet<T> (conjunto);
+			var ret = new Grafo<T> ();
+
+			foreach (var u in AristasColección)
+			{
+				if (col.Contains (u.Origen) || col.Contains (u.Destino))
+					ret.AgregaArista (u);
+			}
+			return ret;
+		}
+
+		bool ILecturaGrafo<T>.this [T desde, T hasta]
+		{
+			get
+			{
+				return ExisteArista (desde, hasta);
+			}
+		}
+
+		float IGrafoPeso<T>.this [T desde, T hasta]
+		{
+			get
+			{ 
+				IArista<T> art;
+				return TryGetArista (desde, hasta, out art) ? art.Peso : 0;
+			}
+			set
+			{
+				IArista<T> art;
+				if (TryGetArista (desde, hasta, out art))
+					AristasColección.Remove (art);
+				AgregaArista (new Arista<T> (desde, hasta, value));
+			}
+		}
+
+		float ILecturaGrafoPeso<T>.this [T desde, T hasta]
+		{
+			get
+			{ 
+				IArista<T> art;
+				return TryGetArista (desde, hasta, out art) ? art.Peso : 0;
+			}
+		}
+
+		public ICollection<T> Nodos
+		{
+			get
+			{
+				var ret = new HashSet<T> ();
+				foreach (var a in AristasColección)
+				{
+					ret.Add (a.Origen);
+					ret.Add (a.Destino);
+				}
+				return ret;
+			}
+		}
+
+
+		#endregion
+
+		public void AgregaArista (IArista<T> art)
+		{
+			AristasColección.Add (art);
+		}
+
+		public IArista<T> GetArista (T desde, T hasta)
+		{
+			foreach (var x in AristasColección)
+			{
+				if (x.Origen.Equals (desde) && x.Destino.Equals (hasta))
+					return x;
+			}
+			throw new AristaInexistenteException<T> (desde, hasta, this);
+		}
+
+		public bool ExisteArista (T desde, T hasta)
+		{
+			foreach (var x in AristasColección)
+			{
+				if (x.Origen.Equals (desde) && x.Destino.Equals (hasta))
+					return true;
+			}
+			return false;
+		}
+
+		public bool TryGetArista (T desde, T hasta, out IArista<T> art)
+		{
+			foreach (var x in AristasColección)
+			{
+				if (x.Origen.Equals (desde) && x.Destino.Equals (hasta))
+				{
+					art = x;
+					return true;
+				}
+			}
+			art = null;
+			return false;
+		}
+	}
+
+
+	[Serializable]
+	public class AristaInexistenteException<T> : Exception
+	{
+		public T Origen;
+		public T Destino;
+		public ILecturaGrafo<T> Grafo;
+
+		public AristaInexistenteException ()
+		{
+		}
+
+		public AristaInexistenteException (T origen,
+		                                   T destino,
+		                                   ILecturaGrafo<T> grafo)
+		{
+			Origen = origen;
+			Destino = destino;
+			Grafo = grafo;
+		}
+
+		public AristaInexistenteException (string message)
+			: base (message)
+		{
+		}
+
+		public AristaInexistenteException (string message, Exception inner)
+			: base (message, inner)
+		{
+		}
+
+		protected AristaInexistenteException (System.Runtime.Serialization.SerializationInfo info,
+		                                      System.Runtime.Serialization.StreamingContext context)
+			: base (info, context)
+		{
+		}
+	}
+}
