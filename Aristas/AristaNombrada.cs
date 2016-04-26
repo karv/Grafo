84dc0be55701e30f89306a3cb086d4c6c4e9842f
Edgar Carballo <karvayoEdgar@gmail.com>
2016-04-26 16:28:57 -0500

    Agregar clase AristaNombrada

---
+++ Aristas/AristaNombrada.cs
@@ -0,0 +1,39 @@
+﻿namespace Graficas.Aristas
+{
+	public class AristaNombrada<TNodo, TNombre>  : IArista<TNodo>
+	{
+		protected AristaNombrada ()
+		{
+		}
+
+		public AristaNombrada (TNodo origen, TNodo destino, TNombre nombre)
+		{
+			Origen = origen;
+			Destino = destino;
+			Nombre = nombre;
+		}
+
+		/// <summary>
+		/// Origen de la arista
+		/// </summary>
+		public TNodo Origen { get; protected set; }
+
+		/// <summary>
+		/// Destino de la arista
+		/// </summary>
+		public TNodo Destino { get; protected set; }
+
+		/// <summary>
+		/// Nombre de la arista
+		/// </summary>
+		public TNombre Nombre { get; protected set; }
+
+		float IArista<TNodo>.Peso
+		{
+			get
+			{
+				return 1;
+			}
+		}
+	}
+}
