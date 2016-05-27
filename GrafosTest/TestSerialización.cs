732e95f9259978666b32ade2a6a1d9e99706c181
Edgar Carballo <karvayoEdgar@gmail.com>
2016-05-27 17:51:02 -0500

    Caminos Serializable

--- GrafosTest/TestSerialización.cs
+++ GrafosTest/TestSerialización.cs
@@ -1,6 +1,7 @@
 ﻿using NUnit.Framework;
 using Graficas.Grafo;
 using Graficas.Continuo;
+using Graficas.Rutas;
 
 namespace Test
 {
@@ -33,6 +34,25 @@ namespace Test
 
 			var gr4 = new HardGrafo<Objeto> (gr);
 			TestSerial (gr4);
+		}
+
+		[Test]
+		public void PathSet ()
+		{
+			var gr = new Grafo<Objeto, float> ();
+			gr [0, 1] = 1;
+			gr [1, 2] = 2;
+			gr [2, 3] = 3;
+
+			var rr = new ConjuntoRutasÓptimas<Objeto> (gr);
+			var zero3 = rr.CaminoÓptimo (0, 3);
+
+			Store.BinarySerialization.WriteToBinaryFile ("some.graph", rr);
+			var rr2 = Store.BinarySerialization.ReadFromBinaryFile <ConjuntoRutasÓptimas<Objeto>> ("some.graph");
+			var copia = rr2.CaminoÓptimo (0, 3);
+			Assert.AreEqual (zero3.NumPasos, copia.NumPasos);
+			Assert.AreEqual (zero3.NodoInicial, copia.NodoInicial);
+			Assert.AreEqual (zero3.NodoFinal, copia.NodoFinal);
 		}
 
 		[Test]
