5d1073e04f206c06869527ff0b89b8888ac4f17b
Edgar Carballo <karvayoEdgar@gmail.com>
2016-06-01 19:50:34 -0500

    idem para Grafo[2]

--- GrafosTest/Graf2.cs
+++ GrafosTest/Graf2.cs
@@ -240,8 +240,10 @@ namespace Test
 			r = gr.CaminoÓptimo (0, 0, z => z.Data);
 			Assert.IsNull (r);
 
-			r = gr.CaminoÓptimo (0, -1, z => z.Data);
-			Assert.IsNull (r);
+			Assert.Throws<NodoInexistenteException> (new TestDelegate (delegate
+			{
+				r = gr.CaminoÓptimo (0, -1, z => z.Data);
+			}));
 		}
 	}
 }
