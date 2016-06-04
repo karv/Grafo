e36e305755ba5a3d6fc2326d29baaeccf9e664e3
Edgar Carballo <karvayoEdgar@gmail.com>
2016-06-04 17:31:28 -0500

    + implementación de un grafo como función x \in f(x) para x \in Nodos. Al contrario de como conjunto de aristas

---
+++ Grafo/GrafoVecindad.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+namespace Graficas.Grafo
+{
+	public class GrafoVecindad<T>
+	{
+		#region Ctor
+
+		public GrafoVecindad (IEqualityComparer<T> comparador = null)
+		{
+			Comparador = comparador ?? EqualityComparer<T>.Default;
+			Nodos = new HashSet<T> (Comparador);
+			Vecindad = new Dictionary<T, HashSet<T>> (Comparador);
+			inicializaDiccionario ();
+		}
+
+		void inicializaDiccionario ()
+		{
+			foreach (var x in Nodos)
+				Vecindad.Add (x, new HashSet<T> (Comparador));
+		}
+
+		#endregion
+
+		#region Interno
+
+		HashSet<T> Nodos { get; }
+
+		public IEqualityComparer<T> Comparador { get; }
+
+		protected Dictionary<T, HashSet<T>> Vecindad { get; }
+
+		#endregion
+	}
+}
