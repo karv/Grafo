ef6464ca6efbc96101068cfb3e36ec130e297083
Edgar Carballo <karvayoEdgar@gmail.com>
2016-06-04 17:36:57 -0500

    Implementar Aristas() y Clear ()

--- Grafo/GrafoVecindad.cs
+++ Grafo/GrafoVecindad.cs
@@ -1,8 +1,9 @@
 ﻿using System.Collections.Generic;
+using Graficas.Aristas;
 
 namespace Graficas.Grafo
 {
-	public class GrafoVecindad<T>
+	public class GrafoVecindad<T> : IGrafo<T>
 	{
 		#region Ctor
 
@@ -29,6 +30,65 @@ namespace Graficas.Grafo
 		public IEqualityComparer<T> Comparador { get; }
 
 		protected Dictionary<T, HashSet<T>> Vecindad { get; }
+
+		public bool Simétrico { get; }
+
+		#endregion
+
+		#region Grafo
+
+		/// <summary>
+		/// Elimina aristas.
+		/// </summary>
+		public void Clear ()
+		{
+			foreach (var x in Vecindad)
+				x.Value.Clear ();
+		}
+
+		/// <summary>
+		/// Colección de aristas existentes.
+		/// </summary>
+		/// <remarks>No preserva referencia </remarks>
+		public ICollection<IArista<T>> Aristas ()
+		{
+			var ret = new HashSet<IArista<T>> ();
+			foreach (var x in Vecindad)
+				foreach (var y in x.Value)
+					ret.Add (new AristaBool<T> (x.Key, y, true, true, Simétrico));
+			return ret;
+		}
+
+		public ICollection<T> Vecinos (T nodo)
+		{
+			throw new System.NotImplementedException ();
+		}
+
+		public Graficas.Rutas.IRuta<T> ToRuta (IEnumerable<T> seq)
+		{
+			throw new System.NotImplementedException ();
+		}
+
+		public IGrafo<T> Subgrafo (IEnumerable<T> conjunto)
+		{
+			throw new System.NotImplementedException ();
+		}
+
+		public IArista<T> this [T desde, T hasta]
+		{
+			get
+			{
+				throw new System.NotImplementedException ();
+			}
+		}
+
+		ICollection<T> IGrafo<T>.Nodos
+		{
+			get
+			{
+				throw new System.NotImplementedException ();
+			}
+		}
 
 		#endregion
 	}
