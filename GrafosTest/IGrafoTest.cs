c6c59ebdc34b5d7993f4f833b636f787c0bb7312
Edgar Carballo <karvayoEdgar@gmail.com>
2016-06-12 17:57:52 -0500

    Otra clase de pruebas

--- GrafosTest/IGrafoTest.cs
+++ GrafosTest/IGrafoTest.cs
@@ -4,7 +4,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using Test;
-using System.Collections;
 
 namespace Test
 {
@@ -13,12 +12,39 @@ namespace Test
 	{
 		public IGrafo<int> Gr { get; set; }
 
-		public abstract void Reset ();
+		static Random r = new Random ();
+
+		public void HacerAleatorio ()
+		{
+			Gr.Clear ();
+			var nods = new List<int> (Gr.Nodos);
+
+			for (int i = 0; i < nods.Count; i++)
+				for (int j = 0; j < nods.Count; j++)
+					SetValue (i, j, r.Next (2) == 1);
+		}
+
+		public void HacerCluster ()
+		{
+			int clusterSize = r.Next (2, 10);
+			Reset (2 * clusterSize);
+			for (int i = 0; i < clusterSize; i++)
+				for (int j = 0; j < clusterSize; j++)
+				{
+					SetValue (i, j, true);
+					SetValue (i + clusterSize, j + clusterSize, true);
+				}
+			SetValue (0, clusterSize, true);
+			SetValue (clusterSize, 0, true);
+		}
+
+		public abstract void Reset (int size);
 
 		[SetUp]
 		public void Setup ()
 		{
-			Reset ();
+			
+			Reset (r.Next (3, 100));
 		}
 
 		public abstract void SetValue (int a, int b, bool r);
@@ -64,13 +90,36 @@ namespace Test
 			Assert.IsEmpty (Gr.Vecinos (1));
 			Assert.IsEmpty (Gr.Aristas ());
 		}
+
+		[Test]
+		public void AristasVsGetter ()
+		{
+			HacerAleatorio ();
+			var ar = Gr.Aristas ();
+			var ls = new List<int> (Gr.Nodos);
+			for (int i = 0; i < ls.Count; i++)
+				for (int j = 0; j < ls.Count; j++)
+				{
+					var a = Gr [i, j];
+					if (a.Existe)
+					{
+						Assert.True (a.Coincide (i, j));
+						Assert.True (ar.Contains (a));
+					}
+					else
+					{
+						Assert.False (ar.Contains (a));
+					}
+
+				}
+		}
 	}
 
 	public class GrafoTest : IGrafoTest
 	{
-		public override void Reset ()
+		public override void Reset (int size)
 		{
-			var nodos = new int[30];
+			var nodos = new int[size];
 			for (int i = 0; i < nodos.Length; i++)
 				nodos [i] = i;
 			Gr = new Grafo<int> (nodos);
@@ -85,9 +134,9 @@ namespace Test
 
 	public class GrafoData : IGrafoTest
 	{
-		public override void Reset ()
+		public override void Reset (int size)
 		{
-			var nodos = new int[30];
+			var nodos = new int[size];
 			for (int i = 0; i < nodos.Length; i++)
 				nodos [i] = i;
 			Gr = new Grafo<int, float> (nodos);
@@ -102,9 +151,9 @@ namespace Test
 
 	public class GrafoVecindad : IGrafoTest
 	{
-		public override void Reset ()
+		public override void Reset (int size)
 		{
-			var nodos = new int[30];
+			var nodos = new int[size];
 			for (int i = 0; i < nodos.Length; i++)
 				nodos [i] = i;
 			Gr = new Grafo<int> (nodos);
