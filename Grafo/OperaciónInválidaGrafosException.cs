84aef6fa084950927d85d7d6a598af4215e3778f
Edgar Carballo <karvayoEdgar@gmail.com>
2016-06-12 17:30:02 -0500

    Agregar pruebas genéricas de IGrafo<int> para Grafo<1>, Grafo<2> y GrafoVecindad

---
+++ Grafo/OperaciónInválidaGrafosException.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Graficas.Grafo
+{
+	[Serializable]
+	public class OperaciónInválidaGrafosException : Exception
+	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="T:OperaciónInválidaGrafosException"/> class
+		/// </summary>
+		public OperaciónInválidaGrafosException ()
+		{
+		}
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="T:OperaciónInválidaGrafosException"/> class
+		/// </summary>
+		/// <param name="message">A <see cref="T:System.String"/> that describes the exception. </param>
+		public OperaciónInválidaGrafosException (string message)
+			: base (message)
+		{
+		}
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="T:OperaciónInválidaGrafosException"/> class
+		/// </summary>
+		/// <param name="message">A <see cref="T:System.String"/> that describes the exception. </param>
+		/// <param name="inner">The exception that is the cause of the current exception. </param>
+		public OperaciónInválidaGrafosException (string message, Exception inner)
+			: base (message, inner)
+		{
+		}
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="T:OperaciónInválidaGrafosException"/> class
+		/// </summary>
+		/// <param name="context">The contextual information about the source or destination.</param>
+		/// <param name="info">The object that holds the serialized object data.</param>
+		protected OperaciónInválidaGrafosException (System.Runtime.Serialization.SerializationInfo info,
+		                                            System.Runtime.Serialization.StreamingContext context)
+			: base (info,
+			        context)
+		{
+		}
+	}
+}
---
+++ GrafosTest/IGrafoTest.cs
@@ -0,0 +1,81 @@
+﻿using NUnit.Framework;
+using Graficas.Grafo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Test;
+using System.Collections;
+
+namespace Test
+{
+	[TestFixture]
+	public abstract class IGrafoTest
+	{
+		public abstract IGrafo<int> ObtenerGrafoNuevo ();
+
+		public static List<int> GeneralNodos { get; }
+
+		[Test]
+		public void SubGrafo ()
+		{
+			var Gr = ObtenerGrafoNuevo ();
+			var r = new Random ();
+			var hs = new List<int> (Gr.Nodos.Where (z => r.Next (2) == 1));
+
+			var g2 = Gr.Subgrafo (hs);
+			Assert.AreEqual (hs.Count, g2.Nodos.Count);
+			for (int i = 0; i < hs.Count; i++)
+				for (int j = 0; j < hs.Count; j++)
+				{
+					var ni = hs [i];
+					var nj = hs [j];
+					Assert.AreEqual (
+						Gr [ni, nj].Existe,
+						g2 [ni, nj].Existe,
+						string.Format ("i == {0}; j == {1}", ni, nj));
+				}
+
+			while (Gr.Nodos.Contains (hs [0]))
+				hs [0]++;
+
+			Assert.Throws<OperaciónInválidaGrafosException> (delegate
+			{
+				Gr.Subgrafo (hs);
+			});
+		}
+	}
+
+	public class GrafoTest : IGrafoTest
+	{
+		public override IGrafo<int> ObtenerGrafoNuevo ()
+		{
+			var nodos = new int[30];
+			for (int i = 0; i < nodos.Length; i++)
+				nodos [i] = i;
+			return new Grafo<int> (nodos);
+		}
+	}
+
+	public class GrafoData : IGrafoTest
+	{
+		public override IGrafo<int> ObtenerGrafoNuevo ()
+		{
+			var nodos = new int[30];
+			for (int i = 0; i < nodos.Length; i++)
+				nodos [i] = i;
+			return new Grafo<int, float> (nodos);
+		}
+	}
+
+	public class GrafoVecindad : IGrafoTest
+	{
+		public override IGrafo<int> ObtenerGrafoNuevo ()
+		{
+			var nodos = new int[30];
+			for (int i = 0; i < nodos.Length; i++)
+				nodos [i] = i;
+			return new GrafoVecindad<int> (nodos);
+		}
+	}
+
+}
