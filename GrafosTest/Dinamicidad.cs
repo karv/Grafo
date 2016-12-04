f33d3ef170180bf615e616267e92dc6bae0714b9
Edgar Carballo <karvayoEdgar@gmail.com>
2016-12-04 15:43:04 -0600

    Test de remove es éxito

--- GrafosTest/Dinamicidad.cs
+++ GrafosTest/Dinamicidad.cs
@@ -21,5 +21,17 @@ namespace Test
 			gr [1, 0] = true;
 			Assert.True (gr [1, 0]);
 		}
+
+		[Test]
+		public void RemoveNode ()
+		{
+			var gr = new GrafoVecindad<int> ();
+			gr.AddNode (0);
+			gr.AddNode (1);
+			gr [0, 1] = true;
+			Assert.True (gr.Vecinos (0).Contains (1));
+			gr.RemoveNode (1);
+			Assert.False (gr.Vecinos (0).Contains (1));
+		}
 	}
 }
