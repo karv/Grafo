b9aece89fd40da3bc95675bdb7cac1c86b493537
Edgar Carballo <karvayoEdgar@gmail.com>
2017-09-23 13:06:50 -0700

    Add a class for  Collection of paths. Add a class (unfinished) for optimal path computation on demand, which stores in memory already computed paths.

---
+++ Rutas/OnDemandOptimalPathCollection.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using CE.Graph.Grafo;
+using CE.Graph.Nodos;
+using CE.Graph.Rutas;
+
+namespace Graficas.Rutas
+{
+	/// <summary>
+	/// Collection of optimal path getter.
+	/// </summary>
+	public class OnDemandOptimalPathCollection<TNode>
+	{
+		public IGraph<TNode> PathFindingGraph { get; }
+
+		public OnDemandOptimalPathCollection (IGraph<TNode> pathFindingGraph)
+		{
+			PathFindingGraph = pathFindingGraph;
+		}
+
+		public IPath<TNode> GetBestPath (INode<TNode> startNode, INode<TNode> endNode)
+		{
+			if (_optCollection.TryGetPath (startNode.Item, endNode.Item, out var optPath))
+				return optPath;
+
+			// Compute the route
+			throw new NotImplementedException ();
+		}
+
+		readonly PathCollection<TNode> _optCollection = new PathCollection<TNode> ();
+	}
+}
---
+++ Rutas/PathCollection.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.ObjectModel;
+using CE.Graph.Rutas;
+using CE.Graph.Nodos;
+
+namespace Graficas.Rutas
+{
+	public class PathCollection<T> : Collection<IPath<T>>
+	{
+		public bool TryGetPath (T startNode, T endNode, out IPath<T> optPath)
+		{
+			optPath = null;
+			// TODO: Make it binary searchable
+			foreach (var path in this)
+			{
+				// TODO: custom equal
+				if (Equals (path.StartNode, startNode) && Equals (path.EndNode, endNode))
+				{
+					optPath = path;
+					return true;
+				}
+			}
+			return false;
+		}
+
+		public bool TryGetPath (INode<T> startNode, INode<T> endNode, out IPath<T> optPath)
+		{
+			optPath = null;
+			// TODO: Make it binary searchable
+			foreach (var path in this)
+			{
+				// TODO: custom equal
+				if (Equals (path.StartNode, startNode.Item) && Equals (path.EndNode, endNode.Item))
+				{
+					optPath = path;
+					return true;
+				}
+			}
+			return false;
+		}
+	}
+}
